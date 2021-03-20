ec01ce26b540e468cb0a7ff0b2d5d1a2bb173f1a
rednafi <redowan.nafi@gmail.com>
2021-03-21 05:09:22 +0600

    Dead letter tales no tale

--- calc/celery.py
+++ calc/celery.py
@@ -1,11 +1,105 @@
 from enum import Enum
+from typing import DefaultDict
 
-from celery import Celery
+from celery import Celery, bootsteps
 from celery.app.log import TaskFormatter
 from celery.signals import after_setup_task_logger
 from kombu import Exchange, Queue
 
 from calc.settings import config
+
+
+class LogColor(str, Enum):
+    START_CYAN = "\033[92m"
+    END_WHITE = "\033[0m"
+
+
+@after_setup_task_logger.connect
+def setup_task_logger(logger, *args, **kwargs):
+    for handler in logger.handlers:
+        handler.setFormatter(
+            TaskFormatter(
+                LogColor.START_CYAN
+                + """
+📗 Async Task Log
+==================
+
+TIMESTAMP   : %(asctime)s
+Task ID     : %(task_id)s
+TASK NAME   : %(task_name)s
+LOG MSG     : %(name)s - %(levelname)s - %(message)s
+"""
+                + LogColor.END_WHITE
+            )
+        )
+
+
+class ExchangeName(str, Enum):
+    ALPHA = "alpha"
+    BETA = "beta"
+    DEAD_LETTER = "dead"
+
+
+class ExchangeType(str, Enum):
+    ALPHA = "direct"
+    BETA = "direct"
+    DEAD_LETTER = "direct"
+
+
+class QueueName(str, Enum):
+    DEFAULT = "default"
+    ANOTHER_0 = "another_0"
+    ANOTHER_1 = "another_1"
+    ANOTHER_2 = "another_2"
+    DEAD_LETTER = "dead_letter"
+
+
+class RouteKeyName(str, Enum):
+    """Routing key attributes have the same names as the queues."""
+
+    DEFAULT = f"{ExchangeName.ALPHA}.{QueueName.DEFAULT}"
+    ANOTHER_0 = f"{ExchangeName.BETA}.{QueueName.ANOTHER_0}"
+    ANOTHER_1 = f"{ExchangeName.BETA}.{QueueName.ANOTHER_1}"
+    ANOTHER_2 = f"{ExchangeName.BETA}.{QueueName.ANOTHER_2}"
+    DEAD_LETTER = f"{ExchangeName.DEAD_LETTER}.{QueueName.DEAD_LETTER}"
+
+
+class DeclareDLXnDLQ(bootsteps.StartStopStep):
+    """
+    Celery Bootstep to declare the Dead Letter exchanges and Dead Letter queues
+    before the worker starts processing tasks.
+
+    Tasks can go to the dead_letter_queue when—
+
+    - Message that is sent to a queue that does not exist.
+    - Queue length limit exceeded.
+    - Message length limit exceeded.
+    - Message is rejected by another queue exchange.
+    - Message reaches a threshold read counter number, because it is not consumed.
+    - Sometimes  this is called a "back out queue".
+    - The message expires due to per-message TTL (time to live)
+
+    """
+
+    requires = {"celery.worker.components:Pool"}
+
+    def start(self, worker):
+        app = worker.app
+
+        # Declare DLX and DLQ
+        exchange_dead_letter = Exchange(
+            ExchangeName.DEAD_LETTER, type=ExchangeType.DEAD_LETTER
+        )
+
+        queue_dead_letter = Queue(
+            QueueName.DEAD_LETTER,
+            exchange_dead_letter,
+            routing_key=RouteKeyName.DEAD_LETTER,
+        )
+
+        with worker.app.pool.acquire() as conn:
+            queue_dead_letter.bind(conn).declare()
+
 
 app = Celery("calc")
 
@@ -15,29 +109,28 @@ app.conf.broker_url = config.CELERY_BROKER_URL
 app.conf.result_backend = config.CELERY_RESULT_BACKEND
 
 # Declaring AMQP exchanges.
-exchange_alpha = Exchange("alpha", type="direct")
-exchange_beta = Exchange("beta", type="direct")
+exchange_alpha = Exchange(ExchangeName.ALPHA, type=ExchangeType.ALPHA)
+exchange_beta = Exchange(ExchangeName.BETA, type=ExchangeType.BETA)
 
 # Declaring AMQP Queues and binding them with the Exchanges.
 queue_default = Queue(
-    name="default",
-    exchange=exchange_alpha,
-    routing_key="alpha.default",
+    QueueName.DEFAULT,
+    exchange_alpha,
+    routing_key=RouteKeyName.DEFAULT,
+    queue_arguments={
+        "x-dead-letter-exchange": ExchangeName.DEAD_LETTER,
+        "x-dead-letter-routing-key": RouteKeyName.DEAD_LETTER,
+    },
 )
+
 queue_another_0 = Queue(
-    name="another_0",
-    exchange=exchange_beta,
-    routing_key="beta.another_0",
+    QueueName.ANOTHER_0, exchange_beta, routing_key=RouteKeyName.ANOTHER_0
 )
 queue_another_1 = Queue(
-    name="another_1",
-    exchange=exchange_beta,
-    routing_key="beta.another_1",
+    QueueName.ANOTHER_1, exchange_beta, routing_key=RouteKeyName.ANOTHER_1
 )
 queue_another_2 = Queue(
-    name="another_2",
-    exchange=exchange_beta,
-    routing_key="beta.another_2",
+    QueueName.ANOTHER_2, exchange_beta, routing_key=RouteKeyName.ANOTHER_2
 )
 
 
@@ -49,15 +142,14 @@ app.conf.task_queues = (
     queue_another_2,
 )
 
-# Declaring default exchange and queue config. This is mostly useless because
-# our clever TaskRouter will throw an exception when an orphan task without properly
-# configured worker and queue appears.
-app.conf.task_default_queue = "default"
-app.conf.task_default_exchange = "alpha"
-app.conf.task_default_routing_key = "alpha.default"
+# Declaring default exchange and queue config. This exchange and queue will be used
+# when a task has unspecified or ill-configured QUEUES_TO_TASKS relationship.
+app.conf.task_default_queue = queue_default.name
+app.conf.task_default_exchange = queue_default.exchange.name
+app.conf.task_default_routing_key = queue_default.routing_key
 
-# Importing the task modules.
-app.conf.imports = ("calc.pkg_1.tasks", "calc.pkg_2.tasks")
+# Add steps to workers that declare DLX and DLQ if they don't exist.
+app.steps["worker"].add(DeclareDLXnDLQ)
 
 
 class RouterConfigError(Exception):
@@ -146,12 +238,19 @@ class RouterMeta(type):
         return super().__new__(metacls, cls, bases, namespace)
 
 
+# Importing the task modules.
+app.conf.imports = ("calc.pkg_1.tasks", "calc.pkg_2.tasks", "calc.pkg_3.tasks")
+
+
 class Router(metaclass=RouterMeta):
     QUEUES_TO_TASKS = {
         queue_default: ("calc.pkg_1.tasks.add",),
         queue_another_0: ("calc.pkg_1.tasks.sub",),
         queue_another_1: ("calc.pkg_2.tasks.mul",),
-        queue_another_2: ("calc.pkg_2.tasks.div",),
+        queue_another_2: (
+            "calc.pkg_2.tasks.div",
+            "calc.pkg_3.tasks.modulo",
+        ),
     }
 
 
@@ -162,31 +261,6 @@ def route_task(name, args, kwargs, options, task=None, **kw):
 
 # Registering the task routers.
 app.conf.task_routes = (route_task,)
-
-
-class LogColor(str, Enum):
-    START_CYAN = "\033[92m"
-    END_WHITE = "\033[0m"
-
-
-@after_setup_task_logger.connect
-def setup_task_logger(logger, *args, **kwargs):
-    for handler in logger.handlers:
-        handler.setFormatter(
-            TaskFormatter(
-                LogColor.START_CYAN
-                + """
-📗 Async Task Log
-==================
-
-TIMESTAMP   : %(asctime)s
-Task ID     : %(task_id)s
-TASK NAME   : %(task_name)s
-LOG MSG     : %(name)s - %(levelname)s - %(message)s
-"""
-                + LogColor.END_WHITE
-            )
-        )
 
 
 # For development: You can use this to run the tasks instantly, without workers.
--- calc/main.py
+++ calc/main.py
@@ -2,7 +2,9 @@ from random import randint
 
 from calc.pkg_1 import chains as chains_1
 from calc.pkg_2 import chains as chains_2
+from calc.pkg_3 import tasks
 
 while True:
     res_1 = chains_1.add_sub(randint(1, 100), randint(101, 200))
     res_2 = chains_2.mul_div(randint(1000, 2000), randint(3000, 4000))
+    res_3 = tasks.modulo.apply_async(args=(randint(0, 1), randint(0, 1)))
